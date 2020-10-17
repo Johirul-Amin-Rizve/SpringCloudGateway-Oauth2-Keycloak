b6a17761f6adeb93703e12b07ec71b6582c9b2d5
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-10-17 18:55:04 +0600

    Initial Commit

---
+++ src/main/java/com/example/samplespringsecurity/SampleSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplespringsecurity/SampleSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
